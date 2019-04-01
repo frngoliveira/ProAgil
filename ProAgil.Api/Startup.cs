624f912d812be5894325bfa71e7782801db64878
Fernando Gomes de Oliveira <fern.fgoliveira@gmail.com>
2019-03-31 21:58:22 -0300

    Adicionado Projeto Angular SPA

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -36,6 +36,8 @@ namespace ProAgil.Api
             services
             .AddMvc()
             .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +53,7 @@ namespace ProAgil.Api
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
